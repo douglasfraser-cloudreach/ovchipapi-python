0319dfab5c4c0ae70657c1b0a294505bfeed6d8f
Hylcos <hylco.uding@student.hu.nl>
2017-02-20 22:03:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from distutils.core import setup
 setup(name='OvStat',
       version='1.0',
       description='OV Chipkaart API',
-      author='',
+      author='Hylco Uding',
       author_email='chipchop@protonmail.com',
-       packages=['ovstat'],
-        install_requires=[
+      packages=['ovstat'],
+      install_requires=[
         "requests",
-    ],
+      ],
+      url = 'https://github.com/OVChip/ovchipapi-python'
+      download_url = 'https://github.com/OVChip/ovchipapi-python/tarball/0.1'
+      keywords = ["OV","Public Transport","ov-chipkaart","chipkaart"]
      )
