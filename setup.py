476a6956cc9afa5a9d6ef10f68fe0051e035b215
Douglas Fraser <douglas.fraser@cloudreach.com>
2018-09-19 08:35:22 +0000

    Auto format

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from distutils.core import setup
 
-setup(name='OvStat',
-      version='1.0',
-      description='OV Chipkaart API',
-      author='Hylco Uding',
-      author_email='chipchop@protonmail.com',
-      packages=['ovstat'],
-      install_requires=[
-        "requests",
-      ],
-      url = 'https://github.com/OVChip/ovchipapi-python',
-      download_url = 'https://github.com/OVChip/ovchipapi-python/tarball/0.1',
-      keywords = ["OV","Public Transport","ov-chipkaart","chipkaart"]
-     )
+setup(
+    name="OvStat",
+    version="1.0",
+    description="OV Chipkaart API",
+    author="Hylco Uding",
+    author_email="chipchop@protonmail.com",
+    packages=["ovstat"],
+    install_requires=["requests"],
+    url="https://github.com/OVChip/ovchipapi-python",
+    download_url="https://github.com/OVChip/ovchipapi-python/tarball/0.1",
+    keywords=["OV", "Public Transport", "ov-chipkaart", "chipkaart"],
+)
